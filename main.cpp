d39d45462a5693d8c9e200de9d61f682882ab097
yayayy <46852937+yayayy@users.noreply.github.com>
2019-01-20 00:17:59 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include "PMT.h"
+#include "PV.h"
+#include "NumberOfPeriods.h"
+#include "Rate.h"
+using namespace std ;
+
+int main() {
+   //CalculPmt();
+    //CalculPV();
+    CalculNumberOfPeriods();
+    //CalculYTM();
+}
+
+
+
